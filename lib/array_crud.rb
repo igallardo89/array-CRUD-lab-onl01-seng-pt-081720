cdb3c5f516327f7e68e0b71f984dacd7e4e8bf96
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-22 19:30:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,19 +24,15 @@ end
 def remove_element_from_end_of_array(array)
   
   create_an_array.pop
-  
 end
 
 def remove_element_from_start_of_array(array)
-  
   create_an_array.shift
-  
 end
 
 def retrieve_element_from_index(array, index_number)
   
   create_an_array[0]
-  
 end
 
 def retrieve_first_element_from_array(array)
@@ -45,7 +41,5 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
-  create_an_array
-  
+  create_an_array.last
 end
